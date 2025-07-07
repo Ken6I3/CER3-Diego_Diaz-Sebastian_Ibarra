bc7bd1a63b372902b45820652369ab471152c8c6
Ken6I3 <sibarra@usm.cl>
2025-07-07 01:55:53 -0400

    Delete Certamen3_TLP directory

--- Certamen3_TLP/core/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Taller
-
-class ProponerTallerForm(forms.ModelForm):
-    class Meta:
-        model = Taller
-        # Incluye todos los campos que el usuario puede llenar, excepto 'estado'
-        exclude = ['estado', 'usuario']  # si tienes campo usuario para registrar quien propone
